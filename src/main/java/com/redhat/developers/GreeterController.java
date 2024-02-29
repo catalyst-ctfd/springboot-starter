7dc7d37cafaa8ec34a92f19baa5baa67e3355932
nmalvankar <nmalvankar@gmail.com>
2024-02-29 12:10:37 -0500

    Update GreeterController.java

--- src/main/java/com/redhat/developers/GreeterController.java
+++ src/main/java/com/redhat/developers/GreeterController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 @Slf4j
@@ -12,7 +13,16 @@ public class GreeterController {
 
 
     @GetMapping("/")
-    public String greet() {
-        return "Hello World";
+    public String greet(HttpServletRequest request) {
+        // Get the hostname from the request
+        String hostname = request.getServerName();
+        String response = null;
+
+        if (hostname.contains("aloha")) {
+            response = "ALOHA_GREETING";
+        else
+            response = "HELLO_WORLD";
+        
+        return response;
     }
 }
